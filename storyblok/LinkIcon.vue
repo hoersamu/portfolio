8621c40d0d9cd21fbfd5018a0f7beb7f13a674c7
Samuel Höra <samuel@hoera.dev>
2023-01-06 20:10:27 +0100

    fix: link icon hover color

--- storyblok/LinkIcon.vue
+++ storyblok/LinkIcon.vue
@@ -1,9 +1,5 @@
 <template>
-	<NuxtLink
-		:to="blok.link.cached_url"
-		class="hover:text-[#50b0ae]"
-		:target="blok.link.target"
-	>
+	<NuxtLink :to="blok.link.cached_url" :target="blok.link.target">
 		<svg-icon
 			:name="blok.icon_name"
 			:title="blok.title"
