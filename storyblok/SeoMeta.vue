ea4affd339b33720c9f0c91e0ef4e11051324016
Samuel Höra <samuel@hoera.dev>
2023-01-06 20:06:19 +0100

    added seo

---
+++ storyblok/SeoMeta.vue
@@ -0,0 +1,30 @@
+<template></template>
+
+<script setup>
+const props = defineProps({ blok: Object });
+
+const meta = [
+	{ name: "charSet", content: "utf-8" },
+	{ name: "description", content: props.blok.description },
+	{ name: "author", content: props.blok.author },
+	{ name: "og:title", content: props.blok.title },
+	{ name: "og:description", content: props.blok.description },
+	{ name: "og:type", content: "website" },
+	{ name: "og:url", content: "https://hoera.dev" },
+	{ name: "og:image", content: props.blok.image.filename },
+	{ name: "twitter:card", content: "summary" },
+	{ name: "twitter:title", content: props.blok.title },
+	{ name: "twitter:description", content: props.blok.description },
+	{ name: "twitter:creator", content: props.blok.author },
+	{ name: "twitter:image", content: props.blok.image.filename },
+];
+
+if (props.blok.keywords) {
+	meta.push({ name: "keywords", content: props.blok.keywords });
+}
+
+useHead({
+	title: props.blok.title,
+	meta,
+});
+</script>
